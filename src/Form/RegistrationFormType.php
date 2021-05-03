b83b38bd3f6c4cfea0779628617a4d88c5641c24
DESKTOP-90HDMMP\Chris <vikingprank@outlook.com>
2021-05-03 19:15:54 +0200

    decomposition form inscription + css flash

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,7 +23,7 @@ class RegistrationFormType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
-            ->add('PlainPassword', PasswordType::class, [
+            ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
