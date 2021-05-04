7ad7b1a4e42ddf72f7b1892db21e0bbb956313fa
NarlexDev <52083826+NarlexDev@users.noreply.github.com>
2021-05-04 14:16:27 +0200

    bootstrap css

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'user_profil')]
+    public function index()
+    {
+        return $this->render('user/index.html.twig');
+}
+}
